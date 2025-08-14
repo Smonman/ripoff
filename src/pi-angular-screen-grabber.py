6fcec87ce52d681f2ce4c7083d16b0b3304f335f
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2025-08-14 17:18:42 +0200

    feat: add logging config and args

--- src/pi-angular-screen-grabber.py
+++ src/pi-angular-screen-grabber.py
@@ -1,10 +1,21 @@
 import argparse
 import logging
+import logging.config
+
+logging.config.fileConfig("src/logging.conf")
+LOGGER = logging.getLogger()
 
 
 def main(args: dict) -> None:
-    pass
+    LOGGER.debug(args)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Capture screenshots of an angular application.")
+    parser.add_argument("-a", "--angular-project-path", nargs=1, default=".")
+    parser.add_argument("-p", "--port", nargs=1)
+    parser.add_argument("-o", "--output-path", nargs=1)
+    parser.add_argument("-s", "--size", nargs=2)
+    parser.add_argument("-t", "--timeout", nargs="?", default=0)
+    parser.add_argument("-i", "--interval", nargs=1, default=10)
+    main(parser.parse_args())
