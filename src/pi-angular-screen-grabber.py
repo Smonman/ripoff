9c13c92508879e99e6710ce1eff99a35d74928b4
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2025-08-14 17:58:57 +0200

    feat: implement basic angular project controls

--- src/pi-angular-screen-grabber.py
+++ src/pi-angular-screen-grabber.py
@@ -1,21 +1,49 @@
 import argparse
 import logging
 import logging.config
+import pathlib
+import signal
+import subprocess
+import time
 
 logging.config.fileConfig("src/logging.conf")
 LOGGER = logging.getLogger()
 
 
+def run_angular_project(path: pathlib.Path, port: int) -> subprocess.Popen:
+    LOGGER.debug(f"starting angular project at {path.absolute()} at port {port}")
+    return subprocess.Popen(["ng", "serve", "--port", str(port)], shell=True, cwd=path.absolute())
+
+
+def stop_angular_project(p: subprocess.Popen) -> None:
+    if p is not None:
+        LOGGER.debug(f"terminating angular project")
+        p.terminate()
+
+
+def wait(seconds: int) -> None:
+    time.sleep(seconds)
+
+
 def main(args: dict) -> None:
     LOGGER.debug(args)
+    try:
+        ng = run_angular_project(pathlib.Path(args.angular_project_path), int(args.port))
+        wait(args.delay)
+        while (True):
+            LOGGER.info("capture screenshot")
+            wait(args.interval)
+    except KeyboardInterrupt:
+        LOGGER.debug(f"captured keyboard interrupt")
+        stop_angular_project(ng)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Capture screenshots of an angular application.")
-    parser.add_argument("-a", "--angular-project-path", nargs=1, default=".")
-    parser.add_argument("-p", "--port", nargs=1)
-    parser.add_argument("-o", "--output-path", nargs=1)
-    parser.add_argument("-s", "--size", nargs=2)
-    parser.add_argument("-t", "--timeout", nargs="?", default=0)
-    parser.add_argument("-i", "--interval", nargs=1, default=10)
+    parser.add_argument("-a", "--angular-project-path", required=True)
+    parser.add_argument("-p", "--port", type=int, default=4200)
+    parser.add_argument("-o", "--output-path", default="./out")
+    parser.add_argument("-s", "--size", type=int, nargs=2)
+    parser.add_argument("-d", "--delay", type=int, default=0)
+    parser.add_argument("-i", "--interval", type=int, default=10)
     main(parser.parse_args())
