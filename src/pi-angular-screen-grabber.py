99b51e14787c3b7f432a2e211ae1c6dd303622de
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2025-08-15 22:45:32 +0200

    feat: write screenshot to given output path

--- src/pi-angular-screen-grabber.py
+++ src/pi-angular-screen-grabber.py
@@ -12,25 +12,25 @@ logging.config.fileConfig("src/logging.conf")
 LOGGER = logging.getLogger()
 
 
-def run_angular_project(path: pathlib.Path, port: int) -> subprocess.Popen:
-    LOGGER.debug(f"starting angular project at {path.absolute()} at port {port}")
+def run_angular(path: pathlib.Path, port: int) -> subprocess.Popen:
+    LOGGER.debug(f"starting angular at {path.absolute()} at port {port}")
     return subprocess.Popen(["ng", "serve", "--port", str(port)], shell=True, cwd=path.absolute())
 
 
-def stop_angular_project(p: subprocess.Popen) -> None:
+def stop_angular(p: subprocess.Popen) -> None:
     if p is not None:
-        LOGGER.debug(f"terminating angular project")
+        LOGGER.debug(f"terminating angular")
         p.terminate()
 
 
 def get_webdriver(width: int, height: int, port: int) -> webdriver.Chrome:
-    LOGGER.debug(f"getting webdriver with window size {width} {height}")
+    LOGGER.debug(f"getting webdriver with window size {width}x{height}")
     return start_webdriver(f"http://localhost:{port}", width, height)
 
 
-def start_webdriver(url: str, width: int = 800, height: int = 460) -> webdriver.Chrome:
+def start_webdriver(url: str, width: int = 256, height: int = 256) -> webdriver.Chrome:
     # TODO: window size does not work
-    LOGGER.debug(f"starting new webdriver with window size {width} {height}")
+    LOGGER.debug(f"starting new webdriver with window size {width}x{height}")
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
     options.add_argument(f"--size={width},{height}")
@@ -49,12 +49,16 @@ def stop_webdriver(driver: webdriver.Chrome) -> None:
         driver.quit()
 
 
-def grab_screenshot(driver: webdriver.Chrome) -> None:
-    # TODO: specify output path
-    driver.get_screenshot_as_file("screenshot.png")
+def grab_screenshot(driver: webdriver.Chrome, dest: pathlib.Path) -> None:
+    LOGGER.debug(f"grabbing screenshot")
+    filepath = dest.joinpath("screenshot.png").resolve().absolute()
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    driver.get_screenshot_as_file(filepath)
+    LOGGER.info(f"saved screenshot under {filepath}")
 
 
 def wait(seconds: int) -> None:
+    LOGGER.debug(f"wait for {seconds} seconds")
     time.sleep(seconds)
 
 
@@ -62,27 +66,28 @@ def main(args: dict) -> None:
     LOGGER.debug(args)
     driver = None
     try:
-        ng = run_angular_project(pathlib.Path(args.angular_project_path), int(args.port))
+        ng = run_angular(pathlib.Path(args.angular_project_path), int(args.port))
         wait(args.delay)
         driver = get_webdriver(args.size[0], args.size[1], args.port)
         wait(args.delay)
         while (True):
             LOGGER.info("capture screenshot")
-            grab_screenshot(driver)
+            grab_screenshot(driver, args.output_path)
             wait(args.interval)
     except KeyboardInterrupt:
-        LOGGER.debug(f"captured keyboard interrupt")
+        LOGGER.info(f"captured keyboard interrupt")
     finally:
-        stop_angular_project(ng)
+        LOGGER.info("stopping angular client and webdriver")
+        stop_angular(ng)
         stop_webdriver(driver)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Capture screenshots of an angular application.")
-    parser.add_argument("-a", "--angular-project-path", required=True)
+    parser.add_argument("-a", "--angular-project-path", type=pathlib.Path, required=True)
     parser.add_argument("-p", "--port", type=int, default=4200)
-    parser.add_argument("-o", "--output-path", default="./out")
-    parser.add_argument("-s", "--size", type=int, nargs=2)
+    parser.add_argument("-o", "--output-path", type=pathlib.Path, default=pathlib.Path("./out"))
+    parser.add_argument("-s", "--size", type=int, nargs=2, default=[800, 480])
     parser.add_argument("-d", "--delay", type=int, default=5)
     parser.add_argument("-i", "--interval", type=int, default=10)
     main(parser.parse_args())
