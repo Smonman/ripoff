3e42a0b5951da3e0dd94ba6db9b15b83d79bd6d1
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2025-08-15 13:40:55 +0200

    feat: add basic screenshot capture mechanism

--- src/pi-angular-screen-grabber.py
+++ src/pi-angular-screen-grabber.py
@@ -6,6 +6,8 @@ import signal
 import subprocess
 import time
 
+from selenium import webdriver
+
 logging.config.fileConfig("src/logging.conf")
 LOGGER = logging.getLogger()
 
@@ -21,21 +23,58 @@ def stop_angular_project(p: subprocess.Popen) -> None:
         p.terminate()
 
 
+def get_webdriver(width: int, height: int, port: int) -> webdriver.Chrome:
+    LOGGER.debug(f"getting webdriver with window size {width} {height}")
+    return start_webdriver(f"http://localhost:{port}", width, height)
+
+
+def start_webdriver(url: str, width: int = 800, height: int = 460) -> webdriver.Chrome:
+    # TODO: window size does not work
+    LOGGER.debug(f"starting new webdriver with window size {width} {height}")
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_argument(f"--size={width},{height}")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-plugins")
+    options.add_argument("--mute-audio")
+    driver = webdriver.Chrome(options=options)
+    LOGGER.debug(f"getting page {url}")
+    driver.get(url)
+    return driver
+
+
+def stop_webdriver(driver: webdriver.Chrome) -> None:
+    if driver:
+        LOGGER.debug(f"quitting webdriver")
+        driver.quit()
+
+
+def grab_screenshot(driver: webdriver.Chrome) -> None:
+    # TODO: specify output path
+    driver.get_screenshot_as_file("screenshot.png")
+
+
 def wait(seconds: int) -> None:
     time.sleep(seconds)
 
 
 def main(args: dict) -> None:
     LOGGER.debug(args)
+    driver = None
     try:
         ng = run_angular_project(pathlib.Path(args.angular_project_path), int(args.port))
         wait(args.delay)
+        driver = get_webdriver(args.size[0], args.size[1], args.port)
+        wait(args.delay)
         while (True):
             LOGGER.info("capture screenshot")
+            grab_screenshot(driver)
             wait(args.interval)
     except KeyboardInterrupt:
         LOGGER.debug(f"captured keyboard interrupt")
+    finally:
         stop_angular_project(ng)
+        stop_webdriver(driver)
 
 
 if __name__ == "__main__":
@@ -44,6 +83,6 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--port", type=int, default=4200)
     parser.add_argument("-o", "--output-path", default="./out")
     parser.add_argument("-s", "--size", type=int, nargs=2)
-    parser.add_argument("-d", "--delay", type=int, default=0)
+    parser.add_argument("-d", "--delay", type=int, default=5)
     parser.add_argument("-i", "--interval", type=int, default=10)
     main(parser.parse_args())
